80303858e40a797237698791df812bb7f975c0f8
Mehmet Yusuf Çalışkan <memetycaliskan@gmail.com>
2025-02-28 22:58:02 +0300

    asd

--- src/app/admin/loading.js
+++ src/app/admin/loading.js
@@ -5,7 +5,7 @@ function Loading() {
     <div>
        <div className="container mx-auto p-4">
   <div className="flex justify-between items-center mb-6">
-    <h1 className="text-2xl font-bold">Blog Yazıları</h1>
+    <h1 className="text-2xl font-bold skeleton w-32 h-10"></h1>
     <button className="btn btn-primary skeleton w-32 h-10"></button>
   </div>
 
